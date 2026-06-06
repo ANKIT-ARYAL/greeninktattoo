9e579a199a95d25ba36acdc844586dca568643f5
Ankit <aryalankit40@gmail.com>
2026-06-06 20:12:02 +0545

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={cn(customHeaderFont.variable, "font-sans", geist.variable)}>
       <body className="bg-black text-white">
-        
+        <title>Green Ink Tattoo - Kathmandu's Premier Tattoo Studio</title>
         {/* Global Background Layer - The ONLY place this should exist */}
         <div className="fixed inset-0 -z-50 bg-[#050505]">
           <div className="absolute inset-0 bg-[url('/hero.jpg')] bg-cover bg-center opacity-10" />
